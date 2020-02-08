d2b530cc6c3601179fa763bdbe17e4e4d45f4260
suxgatov <gcorradiranzani@hotmail.com>
2020-02-08 10:40:41 -0300

    Nodemon

--- Modulo02/src/routes.js
+++ Modulo02/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
-const router = new Router();
+const routes = new Router();
 
 routes.get('/', (req,res) =>{
   return res.json('Hello World!');
 })
 
-module.exports = routes;
+export default routes;
