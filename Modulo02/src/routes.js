bba3b54a713f5a9664b1421476835580705bad26
suxgatov <gcorradiranzani@hotmail.com>
2020-02-10 16:23:23 -0300

    Sequelize and Eslint

--- Modulo02/src/routes.js
+++ Modulo02/src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req,res) =>{
+routes.get('/', (req, res) => {
   return res.json('Hello World!');
-})
+});
 
 export default routes;
