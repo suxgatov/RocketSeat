8ecf713acbb8b57d54f14c5efa04146165479490
suxgatov <gcorradiranzani@hotmail.com>
2020-02-11 16:28:11 -0300

    User migration and model

--- Modulo02/src/routes.js
+++ Modulo02/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json('Hello World!');
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    nome: 'Gustavo Corradi',
+    email: 'gcorradiranzani@hotmail.com',
+    password_hash: '12345',
+    provider: false
+  });
+  return res.json(user);
 });
 
 export default routes;
